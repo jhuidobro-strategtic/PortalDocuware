03998195fe12fb1645857ab0a28680ef74aac2cd
OsmarH19 <115659833+OsmarH19@users.noreply.github.com>
2025-09-19 00:53:04 -0500

    Ajustes Listado

--- src/pages/Documents/index.tsx
+++ src/pages/Documents/index.tsx
@@ -4,6 +4,10 @@ import { Col, Container, Row } from 'reactstrap';
 //import Components
 import BreadCrumb from '../../Components/Common/BreadCrumb';
 import AllTasks from './AllTasks';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "simplebar-react/dist/simplebar.min.css";
+import "flatpickr/dist/themes/material_blue.css";
 
 
 const DashboardDocuments = () => {
