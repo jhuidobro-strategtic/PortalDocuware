78b8f1732d8700fa1d69b2742e8f1672b5729f8e
JefersonAcme <130384057+Jeferson1906@users.noreply.github.com>
2026-05-08 14:15:35 -0500

    Ajuste logo test

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -6,6 +6,7 @@ import { Dropdown, DropdownMenu, DropdownToggle, Form } from 'reactstrap';
 import logoSm from "../assets/images/logo-sm.png";
 import logoDark from "../assets/images/logo-dark.png";
 import logoLight from "../assets/images/logo-light.png";
+import newLogo from "../assets/images/newlogodocuware.png";
 
 //import Components
 import SearchOption from '../components/common/SearchOption';
@@ -75,19 +76,19 @@ const Header = ({ onChangeLayoutMode, layoutModeType, headerClass } : any) => {
                             <div className="navbar-brand-box horizontal-logo">
                                 <Link to="/documents" className="logo logo-dark">
                                     <span className="logo-sm">
-                                        <img src={logoSm} alt="" height="22" />
+                                        <img src={newLogo} alt="Docuware" height="32" />
                                     </span>
                                     <span className="logo-lg">
-                                        <img src={logoDark} alt="" height="17" />
+                                        <img src={newLogo} alt="Docuware" height="32" />
                                     </span>
                                 </Link>
 
                                 <Link to="/documents" className="logo logo-light">
                                     <span className="logo-sm">
-                                        <img src={logoSm} alt="" height="22" />
+                                        <img src={newLogo} alt="Docuware" height="32" />
                                     </span>
                                     <span className="logo-lg">
-                                        <img src={logoLight} alt="" height="17" />
+                                        <img src={newLogo} alt="Docuware" height="32" />
                                     </span>
                                 </Link>
                             </div>
