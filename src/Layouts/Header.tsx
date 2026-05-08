85a494814f7f87b8ae66150bb896a03ee262ed14
JefersonAcme <130384057+Jeferson1906@users.noreply.github.com>
2026-05-08 14:20:14 -0500

    Revert "Ajuste logo test"
    This reverts commit 78b8f1732d8700fa1d69b2742e8f1672b5729f8e.

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -6,7 +6,6 @@ import { Dropdown, DropdownMenu, DropdownToggle, Form } from 'reactstrap';
 import logoSm from "../assets/images/logo-sm.png";
 import logoDark from "../assets/images/logo-dark.png";
 import logoLight from "../assets/images/logo-light.png";
-import newLogo from "../assets/images/newlogodocuware.png";
 
 //import Components
 import SearchOption from '../components/common/SearchOption';
@@ -76,19 +75,19 @@ const Header = ({ onChangeLayoutMode, layoutModeType, headerClass } : any) => {
                             <div className="navbar-brand-box horizontal-logo">
                                 <Link to="/documents" className="logo logo-dark">
                                     <span className="logo-sm">
-                                        <img src={newLogo} alt="Docuware" height="32" />
+                                        <img src={logoSm} alt="" height="22" />
                                     </span>
                                     <span className="logo-lg">
-                                        <img src={newLogo} alt="Docuware" height="32" />
+                                        <img src={logoDark} alt="" height="17" />
                                     </span>
                                 </Link>
 
                                 <Link to="/documents" className="logo logo-light">
                                     <span className="logo-sm">
-                                        <img src={newLogo} alt="Docuware" height="32" />
+                                        <img src={logoSm} alt="" height="22" />
                                     </span>
                                     <span className="logo-lg">
-                                        <img src={newLogo} alt="Docuware" height="32" />
+                                        <img src={logoLight} alt="" height="17" />
                                     </span>
                                 </Link>
                             </div>
